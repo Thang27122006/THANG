aac1abb19702127f4e229f2c70e6f608b1592edb
THANG <tranthang20060903@gmail.com>
2025-05-06 00:53:29 +0700

    Update bai7.cpp

--- bai7.cpp
+++ bai7.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> n;
     for (int i = 0; i<=n; i++) {
         if ( i%2==0) {
-            cout << i << " ";
+            cout << i << " "; // không hiểu
         }
     }
     return 0;
